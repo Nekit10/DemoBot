7c51aff4a98fa56ea25b61993546364bbf199190
Nikita S <nikitaserba@icloud.com>
2019-06-06 02:39:58 +0300

    Error fix

--- demobot.py
+++ demobot.py
@@ -17,6 +17,7 @@
 #    Copyright (c) 2019 Nikita Serba
 
 import json
+import time
 
 from botapi import TelegramBotAPI
 
@@ -45,14 +46,14 @@ def check_return_poll_candidates() -> list:
 
     for update in updates:
         try:
-            if '@chatdemocratic_bot' in update['message'] and 'reply_to_message' in update['message'].keys():
+            if '@chatdemocratic_bot' in update['message']['text'] and 'reply_to_message' in update['message'].keys():
                 result = dict()
                 result['chat_id'] = update['message']['reply_to_message']['chat']['id']
-                result['name'] = update['message']['reply_to_message']['from']['first_name'] + update['message']['reply_to_message']['from']['last_name']
+                result['name'] = update['message']['reply_to_message']['from']['first_name'] + ' ' + update['message']['reply_to_message']['from']['last_name']
                 result['user_id'] = update['message']['reply_to_message']['from']['id']
 
                 candidates += [result]
-        except (NameError, IndexError):
+        except (NameError, IndexError, KeyError):
             pass
 
     return candidates
@@ -66,6 +67,7 @@ def start_poll(chat_id: int, name: str, user_id: int) -> None:
     poll_id = int(response['result']['poll']['id'])
 
     poll_info = dict()
+    poll_info['chat_id'] = chat_id
     poll_info['date'] = response['result']['date']
     poll_info['user_id'] = user_id
     poll_info['name'] = name
@@ -81,12 +83,17 @@ def main_loop() -> None:
         for candidate in candidates:
             start_poll(candidate['chat_id'], candidate['name'], candidate['user_id'])
 
+        remove = []
         for poll_id in polls.keys():
             poll_options = api.get_poll_result(poll_id)
-            if polls[poll_id]['date'] >= 12*3600 and poll_options[0]['voter_count'] > poll_options[1]['voter_count']:
-                api.send_message(polls[[poll_id]['chat_id']], 'Выгоняем ' + polls[poll_id]['name'] + ' по результатам опроса!')
+            if (time.time() - polls[poll_id]['date']) >= 20 and poll_options[0]['voter_count'] > poll_options[1]['voter_count']:
+                api.send_message(polls[poll_id]['chat_id'], 'Выгоняем ' + polls[poll_id]['name'] + ' по результатам опроса!')
                 api.kick_chat_member(polls[poll_id]['chat_id'], polls[poll_id]['user_id'])
-            elif polls[poll_id]['date'] >= 24*3600:
-                del polls[poll_id]
+                remove += [poll_id]
+            elif (time.time() - polls[poll_id]['date']) >= 40:
+                remove += [poll_id]
+
+        for i in remove:
+            del polls[i]
 
         api.save_polls()
