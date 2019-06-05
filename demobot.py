4b283dd9995e55cc30d04abb769cf19da9880479
Nikita S <nikitaserba@icloud.com>
2019-06-06 00:34:39 +0300

    Add bot model

---
+++ demobot.py
@@ -0,0 +1,35 @@
+#    This file is part of DemocraticBot.
+#    https://github.com/Nekit10/DemoBot
+#
+#    DemocraticBot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    DemocraticBot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with DemocraticBot.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    Copyright (c) 2019 Nikita Serba
+
+polls: {}
+
+
+def load_token() -> str:
+    pass
+
+
+def check_return_poll_candidates() -> []:
+    pass
+
+
+def start_poll() -> str:
+    pass
+
+
+def main_loop() -> None:
+    pass
