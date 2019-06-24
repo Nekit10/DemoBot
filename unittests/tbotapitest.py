e6e5676e404324f02fbe20c3b0634014019289bd
Nikita S <nikitaserba@icloud.com>
2019-06-24 17:16:15 +0300

    Unittest update

--- unittests/tbotapitest.py
+++ unittests/tbotapitest.py
@@ -20,7 +20,7 @@ import unittest
 import json
 
 from src import logger
-from src.botapi import TelegramBotAPI, TelegramBotException
+from src.bot2api import Bot2API
 
 
 class LoggerFake:
@@ -50,27 +50,18 @@ class TelegramBotAPITests(unittest.TestCase):
         with open('../devconfig.json', 'r') as f:
             token = json.loads(f.read())['token']
 
-        TelegramBotAPI._POLLS_FILENAME = '../polls.json'
-        cls.botapi = TelegramBotAPI(token, True)
+        cls.botapi = Bot2API(token, True)
         try:
-            cls.botapi.get_new_updates()
             cls.botapi.send_message(TelegramBotAPITests.chat_id,
                                      'Starting testing DemoBot. Please, do NOT DO ANYTHING HERE UNTIL ALL TESTS END. '
                                      'Thx')
         except Exception:
             pass
 
-    def tearDown(self) -> None:
-        try:
-            self.botapi.get_new_updates()
-        except Exception:
-            pass
-
     def test_astart_poll(self):
         response = self.botapi.start_poll(self.chat_id, 'Testing Poll...', ['Yes', 'No'])
-        with open('pollid.tmp', 'w') as f:
-            f.write(response['result']['poll']['id'])
 
+    @unittest.skip('New api does not support this')
     def test_get_poll_options(self):
         with open('pollid.tmp', 'r') as f:
             poll_id = int(f.read())
@@ -78,10 +69,11 @@ class TelegramBotAPITests(unittest.TestCase):
         self.assertEqual(0, options[0]['voter_count'])
         self.assertEqual(0, options[1]['voter_count'])
 
+    @unittest.skip('New api does not support this')
     def test_get_new_updates(self):
         self.botapi.get_new_updates()
 
-    @unittest.skip('This test does not support running automatically')
+    @unittest.skip('New api does not support this')
     def test_get_poll_options_second(self):
         print('Answer NO in this poll. Press enter after voting')
         input()
@@ -94,20 +86,22 @@ class TelegramBotAPITests(unittest.TestCase):
         self.assertEqual(1, options[1]['voter_count'])
 
     def test_send_message_to_wrong_chat(self):
-        self.assertRaises(TelegramBotException, self.botapi.send_message, -1, "Test")
-        self.assertRaises(TelegramBotException, self.botapi.send_error_message, -1, Exception())
-        self.assertRaises(TelegramBotException, self.botapi.start_poll, -1, "Test", ["Yes", "No"])
+        self.assertRaises(ConnectionError, self.botapi.send_message, -1, "Test")
+        self.assertRaises(ConnectionError, self.botapi.send_error_message, -1, Exception())
+        self.assertRaises(ConnectionError, self.botapi.start_poll, -1, "Test", ["Yes", "No"])
 
     def test_poll_no_answers(self):
-        self.assertRaises(TelegramBotException, self.botapi.start_poll, self.chat_id, "Test", [])
+        self.assertRaises(ConnectionError, self.botapi.start_poll, self.chat_id, "Test", [])
 
+    @unittest.skip('New api does not support this')
     def test_send_error_message(self):
-        self.botapi.send_error_message(self.chat_id, TelegramBotException('Test error'))
+        self.botapi.send_error_message(self.chat_id, ConnectionError('Test error'))
 
+    @unittest.skip('New api does not support this')
     def test_save_poll(self):
         self.botapi.save_polls()
 
-    def test_send_message_second(self):
+    def test_zsend_message_second(self):
         self.botapi.send_message(self.chat_id, 'Testing ended. Thx')
 
 
