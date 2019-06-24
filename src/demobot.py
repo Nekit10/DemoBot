79fecfe65331a3f766aedc38d349719cf931dace
Nikita S <nikitaserba@icloud.com>
2019-06-24 21:00:42 +0300

    Bot2API integration (except /report command)

--- src/demobot.py
+++ src/demobot.py
@@ -17,16 +17,19 @@
 #    Copyright (c) 2019 Nikita Serba
 
 import json
+import os
 import time
 
 from src import logger
 from src.syslang import langapi
-from src.botapi import TelegramBotAPI
+from src.bot2api import Bot2API
 from src.sysbugs.bugtrackerapi import report_custom_message
 
 polls: dict = {}
 config: dict = {}
-api: TelegramBotAPI
+api: Bot2API
+
+POLLS_FILENAME: str = 'pollsinfo.json'
 
 
 def init_bot(debug: bool = False):
@@ -36,11 +39,15 @@ def init_bot(debug: bool = False):
     logger.logger.debug('Loading config file (debug = ' + 'True)' if debug else 'False)')
     config = load_config(debug)
     logger.logger.debug('Crating instance of TelegramBotAPI in bot init')
-    api = TelegramBotAPI(config['token'], debug)
+    api = Bot2API(debug)
     logger.logger.debug('Adding report command listener')
     api.add_command_listener('report', report_command_processor)
     logger.logger.debug('Adding lang command listener')
     api.add_command_listener('lang', send_lang_inline)
+    logger.logger.debug('Adding update listener that checks kick candidates and starts poll')
+    api.add_message_listener(process_update_and_start_poll)
+    logger.logger.debug('Adding update listener that updates options of all polls')
+    api.add_message_listener(update_poll_options)
 
 
 def load_config(debug: bool = False) -> dict:
@@ -50,38 +57,23 @@ def load_config(debug: bool = False) -> dict:
         return json.loads(f.read())
 
 
-def check_return_poll_candidates() -> list:
-    global api
+def process_update_and_start_poll(update: dict) -> None:
+    try:
+        logger.logger.trace('Checking new update! (is mention? ' + str(
+            config['bot_username'] in update['message']['text']) + '; is reply? ' + str(
+            'reply_to_message' in update['message'].keys()) + ')\n' + str(update))
+        if config['bot_username'] in update['message']['text'] and 'reply_to_message' in update['message'].keys():
+            chat_id = update['message']['reply_to_message']['chat']['id']
+            name = update['message']['reply_to_message']['from']['first_name']
+            if 'last_name' in update['message']['reply_to_message']['from'].keys():
+                name += ' ' + update['message']['reply_to_message']['from']['last_name']
+            user_id = update['message']['reply_to_message']['from']['id']
 
-    logger.logger.trace('Checking poll candidates')
+            logger.logger.info('Found kick candidate in chat #' + str(chat_id) + ', with name ' + name + '(' + str(user_id) + ')')
 
-    updates = api.get_new_updates()['result']
-    candidates = []
-
-    logger.logger.trace('Got ' + str(len(updates)) + ' updates')
-
-    for update in updates:
-        try:
-            logger.logger.trace('Checking new update! (is mention? ' + str(
-                config['bot_username'] in update['message']['text']) + '; is reply? ' + str(
-                'reply_to_message' in update['message'].keys()) + ')\n' + str(update))
-            if config['bot_username'] in update['message']['text'] and 'reply_to_message' in update['message'].keys():
-                result = dict()
-                result['chat_id'] = update['message']['reply_to_message']['chat']['id']
-                result['name'] = update['message']['reply_to_message']['from']['first_name']
-                if 'last_name' in update['message']['reply_to_message']['from'].keys():
-                    result['name'] += ' ' + update['message']['reply_to_message']['from']['last_name']
-                result['user_id'] = update['message']['reply_to_message']['from']['id']
-
-                logger.logger.info('Found kick candidate in chat #' + str(result['chat_id']) + ', with name ' + result['name'] + '(' + str(result['user_id']) + ')')
-
-                candidates += [result]
-        except (NameError, IndexError, KeyError) as e:
-            logger.logger.trace('Ignored name exception in checking poll candidates: ' + str(e))
-
-    logger.logger.trace('Returning ' + str(len(candidates)) + ' kick candidates')
-
-    return candidates
+            start_poll(chat_id, name, user_id)
+    except (NameError, IndexError, KeyError) as e:
+        logger.logger.trace('Ignored name exception in checking poll candidates: ' + str(e))
 
 
 def start_poll(chat_id: int, name: str, user_id: int) -> None:
@@ -89,21 +81,49 @@ def start_poll(chat_id: int, name: str, user_id: int) -> None:
 
     response = api.start_poll(chat_id, langapi.msg_kick(chat_id).replace('%NAME%', name), [langapi.msg_kick_yes(chat_id), langapi.msg_kick_no(chat_id)])
 
-    poll_id = int(response['result']['poll']['id'])
+    poll_id = int(response['poll']['id'])
 
     poll_info = dict()
     poll_info['chat_id'] = chat_id
-    poll_info['date'] = response['result']['date']
+    poll_info['date'] = response['date']
     poll_info['user_id'] = user_id
     poll_info['name'] = name
+    poll_info['options'] = response['poll']['options']
 
     polls[poll_id] = poll_info
 
 
-def check_kick_candidates():
-    candidates = check_return_poll_candidates()
-    for candidate in candidates:
-        start_poll(candidate['chat_id'], candidate['name'], candidate['user_id'])
+def load_polls_info():
+    global polls
+
+    path = os.path.join(os.path.dirname(__file__), '../', POLLS_FILENAME)
+
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            dict_ = json.loads(f.read())
+            polls = {}
+            for key, value in dict_:
+                polls[int(key)] = value
+
+
+def save_polls():
+    path = os.path.join(os.path.dirname(__file__), '../', POLLS_FILENAME)
+
+    if os.path.exists(path):
+        with open(path, 'w') as f:
+            f.write(json.dumps(polls))
+
+
+def update_poll_options(update: dict):
+    global polls
+
+    try:
+        id_ = int(update['poll']['id'])
+        if id_ in polls.keys():
+            polls[id_]['options'] = update['poll']['options']
+        save_polls()
+    except (NameError, KeyError, IndexError):
+        pass
 
 
 def kick_candidate(poll_id: int):
@@ -123,7 +143,7 @@ def check_old_polls():
 
     for poll_id in polls.keys():
         logger.logger.trace('Checking poll with id ' + str(poll_id))
-        poll_options = api.get_poll_result(poll_id)
+        poll_options = polls[poll_id]['options']
         if (time.time() - polls[poll_id]['date']) >= 12 * 3600 and poll_options[0]['voter_count'] > poll_options[1]['voter_count']:
             kick_candidate(poll_id)
             remove += [poll_id]
@@ -143,7 +163,7 @@ def report_command_processor(chat_id: int, from_id: int):
     bug_report_msg = ''
 
     while bug_report_msg == '':
-        updates_ = api.get_new_updates()['result']
+        updates_ = api.get_new_updates()
         for update_ in updates_:
             try:
                 if update_['message']['chat']['id'] == chat_id and update_['message']['from']['id'] == from_id:
@@ -160,7 +180,7 @@ def report_command_processor(chat_id: int, from_id: int):
     from_msg = ''
 
     while from_msg == '':
-        updates_ = api.get_new_updates()['result']
+        updates_ = api.get_new_updates()
         for update_ in updates_:
             try:
                 if update_['message']['chat']['id'] == chat_id and update_['message']['from']['id'] == from_id:
@@ -184,14 +204,13 @@ def change_lang_in_chat(chat_id: int, lang: str):
 
 def send_lang_inline(chat_id: int, from_id: int):
     logger.logger.info('Sending inline lang chooser in chat #' + str(chat_id))
-    api.send_inline_question(chat_id, langapi.msg_lang_choose(chat_id), langapi.get_all_langs(), change_lang_in_chat)
+    api.send_inline_message(chat_id, langapi.msg_lang_choose(chat_id), langapi.get_all_langs(), change_lang_in_chat)
 
 
 def main_loop() -> None:
     global polls
     logger.logger.info('Started main loop')
+    load_polls_info()
 
     while True:
-        check_kick_candidates()
         check_old_polls()
-        api.save_polls()
